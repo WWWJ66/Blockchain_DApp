d6af7974b2240b63bbb60848b079abec092fc508
王智 <1848195475@qq.com>
2024-12-21 13:54:11 +0800

    更新信息显示

--- frontend/src/components/QueryView.vue
+++ frontend/src/components/QueryView.vue
@@ -22,23 +22,13 @@
       </tr>
       </tbody>
     </table>
-    <div class="result-info">
-      <p v-if="!details.length">暂无查询结果</p>
-      <div v-else>
-        <table class="details-table" v-for="detail in details" :key="detail.id">
-          <thead>
-          <tr>
-            <th>产品名称</th>
-            <th>详细信息</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr v-for="info in detail" :key="info.id">
-            <td>{{ info.name }}</td>
-            <td>{{ info.info }}</td>
-          </tr>
-          </tbody>
-        </table>
+    <div class="no-result-info" v-if="JSON.stringify(details) === '{}'">暂无查询结果</div>
+    <div class="result-info" v-else>
+      <div class="info" v-for="(detail, infoType) in details" :key="infoType">
+        <h3>{{ infoType }}</h3>
+        <div v-for="(value, key) in detail" :key="key">
+          {{ key }}: {{ value }}
+        </div>
       </div>
     </div>
   </div>
@@ -53,7 +43,7 @@ const username = "111111";
 // 定义搜索查询和搜索结果的响应式变量
 const searchQuery = ref('');
 const products = ref([]);
-const details = ref([]);
+const details = ref({});
 
 const getProductsList = async () => {
   console.log(`查询 ${username} 的产品列表`);
@@ -73,15 +63,42 @@ const viewProduct = async (traceCode) => {
   // const res = await axios.post("localhost:9090/getProductInfo", {tracecode: traceCode});
   // searchResults.value = res.data;
   console.log(`展示产品详情: ${traceCode}`);
-  details.value = [[
-    {id: 1, name: '产品A', info: '详细信息A'},
-    {id: 2, name: '产品B', info: '详细信息B'},
-    {id: 3, name: '产品C', info: '详细信息C'}
-  ],[
-    {id: 4, name: '产品1', info: '详细信息A'},
-    {id: 5, name: '产品2', info: '详细信息B'},
-    {id: 6, name: '产品3', info: '详细信息C'}
-  ]];
+  details.value = {
+    "basicInfo": {
+      "name": "黄鱼",
+      "fisherName": "渔民A",
+      "fishedTime": "2024.1.1",
+      "fishedArea": "黄海",
+      "tradeId": "",
+      "tradeTime": ''
+    },
+    "factoryInfo": {
+      "name": "黄鱼酥",
+      "factoryName": "加工厂A",
+      "processTime": "2024.1.2",
+      "factoryAddress": "A市B区",
+      "tradeId": "",
+      "tradeTime": ''
+    },
+    "transportInfo": {
+      "name": "黄鱼酥",
+      "driverName": "司机A",
+      "transportTime": "2024.1.3",
+      "destination": "B市A区",
+      "tradeId": "",
+      "tradeTime": ''
+    },
+    "shopInfo": {
+      "name": "黄鱼酥",
+      "shopName": "商店A",
+      "storageTime": "2024.1.4",
+      "sellTime": "2024.1.4",
+      "tradeId": "",
+      "tradeTime": ''
+    }
+
+  };
+  console.log(details.value);
 };
 
 onMounted(async () => {
@@ -104,66 +121,78 @@ h1 {
   display: flex;
   justify-content: center;
   margin-bottom: 20px;
-}
 
-.search-bar input {
-  padding: 10px;
-  margin-right: 10px;
-  width: 300px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
+  input {
+    padding: 10px;
+    margin-right: 10px;
+    width: 300px;
+    border: 1px solid #ccc;
+  }
 
-.search-bar button {
-  padding: 10px 20px;
-  background-color: #5eaaff;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
+  button {
+    padding: 10px 20px;
+    background-color: #5eaaff;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
 }
 
 .search-bar button:hover {
   background-color: #1d89fd;
 }
 
-.result-info {
+.results-table {
+  border-radius: 4px;
+  width: 400px;
+  margin: 10px auto;
+  border-collapse: collapse;
+  text-align: center;
+
+  th {
+    background-color: #d2ffff;
+    padding: 10px;
+    text-align: center;
+  }
+
+  td {
+    padding: 7px;
+    background-color: rgba(204, 226, 255, 0.7);
+    border-bottom: 1px solid #ddd;
+    justify-content: center;
+
+    button {
+      padding: 8px 15px;
+      background-color: #72b4ff;
+      color: white;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+  }
+}
+
+.results-table td button:hover {
+  background-color: #1e77d8;
+}
+
+.no-result-info {
   margin-top: 40px;
   justify-content: center;
   text-align: center;
   display: flex;
 }
 
-.results-table, .details-table {
-  width: 400px;
-  margin: 0 auto;
-  border-collapse: collapse;
-  text-align: center;
+.info {
+  justify-content: left;
+  text-align: left;
+  h3 {
+    margin-left:20%;
+  }
+  div {
+    margin-left: 30%;
+  }
 }
 
-.results-table th, .details-table th {
-  background-color: #d2ffff;
-  padding: 10px;
-  text-align: center;
-}
-
-.results-table td, .details-table td {
-  padding: 7px;
-  background-color: rgba(204, 226, 255, 0.7);
-  border-bottom: 1px solid #ddd;
-  justify-content: center;
-}
-
-.results-table td button{
-  padding: 8px 15px;
-  background-color: #72b4ff;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-.results-table td button:hover, .details-table td button:hover {
-  background-color: #1e77d8;
-}
 </style>
